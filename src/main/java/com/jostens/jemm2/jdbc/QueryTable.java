7681334f63893706905c23ae461f99a402165c8a
wshromoff <44078915+wshromoff@users.noreply.github.com>
2018-12-28 17:40:30 -0600

    New SQL added and test case working to access Oracle tables

--- src/main/java/com/jostens/jemm2/jdbc/QueryTable.java
+++ src/main/java/com/jostens/jemm2/jdbc/QueryTable.java
@@ -14,10 +14,15 @@ public class QueryTable
 		{
 			Class.forName("oracle.jdbc.driver.OracleDriver");
 
-			String url = "jdbc:oracle:thin:@db_tstt.jostens.com:1565/tstt";
-			String username = "xt11781";
-			String password = "Tigers30";
-			
+//			String url = "jdbc:oracle:thin:@db_tstt.jostens.com:1565/tstt";
+//			String username = "xt11781";
+//			String password = "Tigers30";
+
+			String url = "jdbc:oracle:thin:@devapps:1521/orcl";
+			String username = "jemm2";
+			String password = "Jostens01.";
+
+
 			conn = DriverManager.getConnection(url, username, password);
 			conn.setAutoCommit(false);		// All connections require a user to commit the transaction
 			
