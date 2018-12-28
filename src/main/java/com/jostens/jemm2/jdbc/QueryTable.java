f4cc0cf32939c05f6eca25962718af0e9d8e5390
wshromoff <44078915+wshromoff@users.noreply.github.com>
2018-12-28 14:20:10 -0600

    Addition of test SQL class and unit test

---
+++ src/main/java/com/jostens/jemm2/jdbc/QueryTable.java
@@ -0,0 +1,43 @@
+package com.jostens.jemm2.jdbc;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+
+public class QueryTable
+{
+	public void performQuery()
+	{
+		Connection conn = null;
+		try
+		{
+			Class.forName("oracle.jdbc.driver.OracleDriver");
+
+			String url = "jdbc:oracle:thin:@db_tstt.jostens.com:1565/tstt";
+			String username = "xt11781";
+			String password = "Tigers30";
+			
+			conn = DriverManager.getConnection(url, username, password);
+			conn.setAutoCommit(false);		// All connections require a user to commit the transaction
+			
+			String sql = "select count(*) from TEST";
+			PreparedStatement ps = conn.prepareStatement(sql);
+			ResultSet rs = ps.executeQuery();
+			if (rs.next())
+			{
+				int rowCount = rs.getInt(1);
+				System.out.println("Rows found=" + rowCount);
+			}
+			rs.close();
+			ps.close();
+			conn.close();
+	
+		} 
+		catch (Exception e)
+		{
+			e.printStackTrace();
+		}
+	}
+
+}
---
+++ src/test/java/com/jostens/jemm2/jdbc/QueryTableTest.java
@@ -0,0 +1,15 @@
+package com.jostens.jemm2.jdbc;
+
+import org.junit.Test;
+
+public class QueryTableTest
+{
+
+	@Test
+	public void test()
+	{
+		QueryTable query = new QueryTable();
+		query.performQuery();
+	}
+
+}
