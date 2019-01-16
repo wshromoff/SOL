c5a793074bf87ced199aa41f6e1c0567af885fe4
wshromoff <44078915+wshromoff@users.noreply.github.com>
2019-01-16 16:47:38 -0600

    Switch to 3.0 web project

--- Web/src/main/java/com/waspr/web/Logging.java
+++ Web/src/main/java/com/waspr/web/Logging.java
@@ -3,6 +3,7 @@ package com.waspr.web;
 import java.io.IOException;
 
 import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -10,7 +11,7 @@ import javax.servlet.http.HttpServletResponse;
 /**
  * Servlet implementation class Logging
  */
-//@WebServlet("/StatusAutomations")
+@WebServlet("/logging")
 public class Logging extends HttpServlet {
 	private static final long serialVersionUID = 1L;
        
