065ca84cba00a3c8baf367aa4e2304ed5b6cfa69
wshromoff <44078915+wshromoff@users.noreply.github.com>
2019-01-18 18:56:16 -0600

    Initial SQL script creation

---
+++ Files/SQL/Jemm2/02_Create/CreateAllObjects.sql
@@ -0,0 +1,21 @@
+--
+-- Create all JEMM2 Database Objects
+--
+-- This is just a quick post to share these 2 little shortcuts to convert text from lower case to uppercase and viceversa in Eclipse.
+-- Lower case: CTRL+SHIFT+Y (CMD+SHIFT+Y on Mac OS X)
+-- Upper case: CTRL+SHIFT+X (CMD+SHIFT+X on Mac OS X)
+
+
+
+--
+-- DESIGN
+-- 		This table is an asset design, the lowest level object
+--
+DROP TABLE DESIGN;
+CREATE TABLE DESIGN 
+(
+	ID NUMBER NOT NULL ENABLE,
+	NAME VARCHAR2(50 BYTE) NOT NULL ENABLE,
+	
+	CONSTRAINT "DESIGN_PK" PRIMARY KEY ("ID")
+);
