3fb0f9ee08ccd859907830998cdf3dcae7d62c83
wshromoff <44078915+wshromoff@users.noreply.github.com>
2019-01-22 00:18:31 -0600

    Update SQL files

---
+++ Files/SQL/Worksheets/sheet1.sql
@@ -0,0 +1,8 @@
+select count(*) from design;
+select count(*) from keyword;
+select count(*) from design_keyword;
+
+select b.keyword, count(*) from design_keyword a
+inner join keyword b on b.id = a.keyword
+group by b.keyword
+order by count(*) desc;
