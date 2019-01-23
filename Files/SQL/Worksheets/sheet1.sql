e2bfad59ef7b07e6d3e9f976cf1a33ca73a10592
wshromoff <44078915+wshromoff@users.noreply.github.com>
2019-01-23 17:31:09 -0600

    Database is fully populated with complete Design rows

--- Files/SQL/Worksheets/sheet1.sql
+++ Files/SQL/Worksheets/sheet1.sql
@@ -3,6 +3,6 @@ select count(*) from keyword;
 select count(*) from design_keyword;
 
 select b.keyword, count(*) from design_keyword a
-inner join keyword b on b.id = a.keyword
+inner join keyword b on b.id = a.keyword_id
 group by b.keyword
 order by count(*) desc;
